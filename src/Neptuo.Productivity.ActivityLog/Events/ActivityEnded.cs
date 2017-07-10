90b010bc501ad926783dd5ab8d804107647af77d
Marek Fišera <mara@neptuo.com>
2017-07-10 18:57:39 +0200

    Add main domain events.

---
+++ src/Neptuo.Productivity.ActivityLog/Events/ActivityEnded.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.ActivityLog.Events
+{
+    /// <summary>
+    /// An event raised when an activity has ended.
+    /// </summary>
+    public class ActivityEnded : Event
+    {
+        public string ApplicationPath { get; private set; }
+        public string WindowTitle { get; private set; }
+        public DateTime EndedAt { get; private set; }
+
+        public ActivityEnded(string applicationPath, string windowTitle, DateTime endedAt)
+        {
+            ApplicationPath = applicationPath;
+            WindowTitle = windowTitle;
+            EndedAt = endedAt;
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.ActivityLog/Events/ActivityStarted.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.ActivityLog.Events
+{
+    /// <summary>
+    /// An event raised when a new activity has started.
+    /// </summary>
+    public class ActivityStarted : Event
+    {
+        public string ApplicationPath { get; private set; }
+        public string WindowTitle { get; private set; }
+        public DateTime StartedAt { get; private set; }
+
+        public ActivityStarted(string applicationPath, string windowTitle, DateTime startedAt)
+        {
+            ApplicationPath = applicationPath;
+            WindowTitle = windowTitle;
+            StartedAt = startedAt;
+        }
+    }
+}
