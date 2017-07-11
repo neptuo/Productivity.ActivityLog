4a8ec43614b1c5c1366a358baf10e22349723449
Marek Fišera <mara@neptuo.com>
2017-07-11 22:38:34 +0200

    Producing domain events.

---
+++ src/Neptuo.Productivity.ActivityLog/DomainService.ProcessInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.ActivityLog
+{
+    partial class DomainService
+    {
+        private class ProcessInfo
+        {
+            public string Path { get; set; }
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.ActivityLog/DomainService.cs
@@ -0,0 +1,70 @@
+﻿using Neptuo.Events;
+using Neptuo.Productivity.ActivityLog.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.ActivityLog
+{
+    public partial class DomainService : DisposableBase
+    {
+        private readonly IEventDispatcher eventDispatcher;
+        private readonly ProcessMonitor monitor;
+        private readonly Dictionary<int, ProcessInfo> processCache = new Dictionary<int, ProcessInfo>();
+
+        public DomainService(IEventDispatcher eventDispatcher)
+        {
+            Ensure.NotNull(eventDispatcher, "eventDispatcher");
+            this.eventDispatcher = eventDispatcher;
+
+            monitor = new ProcessMonitor();
+            monitor.Changed += OnProcessChanged;
+            monitor.Start();
+        }
+
+        private async void OnProcessChanged(object sender, ProcessChangedEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            if (e.Type == ProcessChangedType.Process)
+            {
+                if (e.OriginalProcessId != null)
+                {
+                    ProcessInfo originalInfo = GetInfo(e.OriginalProcessId.Value);
+                    await eventDispatcher.PublishAsync(new ActivityEnded(originalInfo.Path, e.OriginalTitle, now));
+                }
+
+                ProcessInfo currentInfo = GetInfo(e.CurrentProcessId);
+                await eventDispatcher.PublishAsync(new ActivityStarted(currentInfo.Path, e.CurrentTitle, now));
+            }
+            else if (e.Type == ProcessChangedType.Title)
+            {
+                ProcessInfo info = GetInfo(e.CurrentProcessId);
+                await eventDispatcher.PublishAsync(new ActivityEnded(info.Path, e.OriginalTitle, now));
+                await eventDispatcher.PublishAsync(new ActivityStarted(info.Path, e.CurrentTitle, now));
+            }
+        }
+
+        private ProcessInfo GetInfo(int processId)
+        {
+            if (!processCache.TryGetValue(processId, out ProcessInfo info))
+            {
+                Process process = Process.GetProcessById(processId);
+                processCache[processId] = info = new ProcessInfo
+                {
+                    Path = process.MainModule?.FileName,
+                };
+            }
+
+            return info;
+        }
+
+        protected override void DisposeManagedResources()
+        {
+            base.DisposeManagedResources();
+            monitor.Dispose();
+        }
+    }
+}
