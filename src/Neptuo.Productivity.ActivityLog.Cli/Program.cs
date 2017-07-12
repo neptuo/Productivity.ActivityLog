cc06b93ca0063e61332973c261658b8e79d46439
Marek Fišera <mara@neptuo.com>
2017-07-12 23:14:53 +0200

    Simple CLI showing domain events.

---
+++ src/Neptuo.Productivity.ActivityLog.Cli/Program.cs
@@ -0,0 +1,58 @@
+﻿using Neptuo.Events;
+using Neptuo.Events.Handlers;
+using Neptuo.Productivity.ActivityLog.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.ActivityLog.Cli
+{
+    class Program : IEventHandler<ActivityStarted>, IEventHandler<ActivityEnded>
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            DefaultEventManager eventManager = new DefaultEventManager();
+            eventManager.AddAll(program);
+
+            DomainService service = new DomainService(eventManager);
+
+            while (true)
+            {
+                Console.WriteLine("Press Esc to exit...");
+                WriteSeparator();
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Escape)
+                    break;
+            }
+
+            service.Dispose();
+            Console.WriteLine("Exiting now...");
+            WriteSeparator();
+        }
+
+        public Task HandleAsync(ActivityStarted payload)
+        {
+            Console.WriteLine($"App: {Path.GetFileName(payload.ApplicationPath)}");
+            Console.WriteLine($"Title: {payload.WindowTitle}");
+            Console.WriteLine($"StartedAt: {payload.StartedAt.ToLongTimeString()}");
+            return Task.CompletedTask;
+        }
+
+        public Task HandleAsync(ActivityEnded payload)
+        {
+            Console.WriteLine($"EndedAt: {payload.EndedAt.ToLongTimeString()}");
+            WriteSeparator();
+            return Task.CompletedTask;
+        }
+
+        private static void WriteSeparator()
+        {
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
