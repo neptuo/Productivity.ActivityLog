4b8470c65f6e35310684eebfc932d009fd9c232a
Marek Fišera <mara@neptuo.com>
2017-07-18 17:45:28 +0200

    Include version info.

--- src/Neptuo.Productivity.ActivityLog.UI/Properties/AssemblyInfo.cs
+++ src/Neptuo.Productivity.ActivityLog.UI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Neptuo.Productivity.ActivityLog.UI;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -8,9 +9,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Neptuo.Productivity.ActivityLog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Productivity: ActivityLog")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Neptuo")]
 [assembly: AssemblyProduct("Neptuo.Productivity.ActivityLog")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +52,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.Preview)]
---
+++ src/Neptuo.Productivity.ActivityLog.UI/Properties/VersionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace Neptuo.Productivity.ActivityLog.UI
+{
+    public class VersionInfo
+    {
+        internal const string Version = "1.0.0";
+        internal const string Preview = "-beta1";
+
+        public static Version GetVersion()
+        {
+            return new Version(Version);
+        }
+    }
+}
