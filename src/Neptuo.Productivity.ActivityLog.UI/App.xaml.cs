6f496a0fd7596d78d9009fcbceecc183a6985f1c
Marek Fišera <mara@neptuo.com>
2017-07-20 22:08:21 +0200

    Include persistence in UI.

--- src/Neptuo.Productivity.ActivityLog.UI/App.xaml.cs
+++ src/Neptuo.Productivity.ActivityLog.UI/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using Neptuo.Events;
+using Neptuo.Formatters;
+using Neptuo.Productivity.ActivityLog.Data;
+using Neptuo.Productivity.ActivityLog.Events;
+using Neptuo.Productivity.ActivityLog.Formatters;
 using Neptuo.Productivity.ActivityLog.Services;
 using Neptuo.Productivity.ActivityLog.ViewModels;
 using Neptuo.Productivity.ActivityLog.Views;
@@ -7,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
@@ -38,26 +43,53 @@ namespace Neptuo.Productivity.ActivityLog
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            DefaultEventManager eventManager = new DefaultEventManager();
+
             dispatcher = new DispatcherHelper(Dispatcher);
             timer = new Timer(1000);
             timer.Elapsed += OnTimerElapsed;
             timer.Start();
 
             OverviewViewModel viewModel = new OverviewViewModel(
-                this, 
-                this, 
-                new DateTimeProvider(), 
+                this,
+                this,
+                new DateTimeProvider(),
                 new ApplicationNameProvider()
             );
 
-            DefaultEventManager eventManager = new DefaultEventManager();
             eventManager.AddAll(viewModel);
 
             service = new DomainService(eventManager);
 
+            SimpleFormatter formatter = new SimpleFormatter();
             MainWindow wnd = new MainWindow();
             wnd.DataContext = viewModel;
+
+            //string todayFile = GetFileName(DateTime.Today);
+            //if (File.Exists(todayFile))
+            //{
+            //    using (Stream file = File.OpenRead(todayFile))
+            //    {
+            //        IDeserializerContext context = new DefaultDeserializerContext(typeof(IEvent));
+            //        while (formatter.TryDeserialize(file, context))
+            //        {
+            //            if (context.Output is ActivityStarted started)
+            //                eventManager.PublishAsync(started).Wait();
+            //            else if (context.Output is ActivityEnded ended)
+            //                eventManager.PublishAsync(ended).Wait();
+            //        }
+            //    }
+            //}
+
+            FileEventStore store = new FileEventStore(formatter, GetFileName);
+            eventManager.AddAll(new EventStoreHandler(store));
+
             wnd.Show();
+        }
+
+        private static string GetFileName(DateTime dateTime)
+        {
+            return $"{dateTime.ToString("yyyy-MM-dd")}.alog";
         }
 
         protected override void OnExit(ExitEventArgs e)
